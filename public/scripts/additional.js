4e19f03f32f0b20d9b9239c9432bbf878f5151f8
todaycode <onetoday1030@gmail.com>
2021-04-06 21:27:18 +0300

    guide tabs toggle classes

--- public/scripts/additional.js
+++ public/scripts/additional.js
@@ -1,5 +1,10 @@
 $(() => {
     $('#guide .mdl-navigation__link').click(e => {
+        $('#guide .mdl-navigation__link').removeClass('bg-success');
+        $('#guide .mdl-navigation__link').removeClass('text-light');
+        e.target.classList.add('bg-success');
+        e.target.classList.add('text-light');
+
         $('#guide .page-content > div').hide();
         setTimeout(() => {$(`#${$(e.target).attr('id').slice(0,-5)}`).show();}, 100)
     })
